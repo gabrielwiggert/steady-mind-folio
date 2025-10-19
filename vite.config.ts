4039c9522f32ddff82920ad33f5a4196b958baa2
Gabriel Wiggert <gabriel.wiggert@w1consultoria.com.br>
2025-10-19 19:34:57 -0300

    Fix: vite.config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/steady-mind-folio/",
   server: {
     host: "::",
     port: 8080,
